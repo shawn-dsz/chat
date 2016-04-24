888f009f788eee2b8938e10e3d552b8904771eab
shawn-dsz <shawn.dsouza@outlook.com>
2016-04-24 10:33:52 +1000

    init

---
+++ script1.js
@@ -0,0 +1,1 @@
+module.exports = 'hi here from 1';
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: {
+    main: [
+      './script1.js',
+      './script2.js'
+    ]
+  },
+  output: {
+    filename: './public/[name].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }
+    ]
+  }
+}
